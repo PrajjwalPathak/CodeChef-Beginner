95f3b093ef447580d7be1f07a032502c350ed2ac
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-09-03 11:38:11 +0530

    Modified INTEST.cpp

--- INTEST.cpp
+++ INTEST.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 using namespace std;
 int main() {
+    //Initialising Variables
     int n=0,k=0,i=0,count=0;
+    //User Input
     scanf("%d %d",&n,&k);
+    //Array
     int a[n];
+    //Forloop for taking input and storing the sum of inputs in the array a[n]
     for(i=0;i<n;i++) {
         scanf("%d",&a[i]);
         if(a[i]%k==0) {
             count+=1;
         }
     }
+    //Printing Result
     printf("%d",count);
     return 0;
     
