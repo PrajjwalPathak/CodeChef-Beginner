65c2bd4e1c33b7eb1a5163a2f53a98dd5e89a0da
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-09-03 11:30:55 +0530

    Added INTEST.cpp program

---
+++ INTEST.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n=0,k=0,i=0,count=0;
+    scanf("%d %d",&n,&k);
+    int a[n];
+    for(i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+        if(a[i]%k==0) {
+            count+=1;
+        }
+    }
+    printf("%d",count);
+    return 0;
+    
+}
