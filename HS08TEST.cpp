49aeae9f491405ce3c4d5fa0445072a435c4d57c
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-09-07 10:54:16 +0530

    Added HS08TEST.cpp program

---
+++ HS08TEST.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x=0;
+    float b=0;
+    scanf("%d %f",&x,&b);
+    if(x%5==0 && x<=(b-0.50)) {
+        b=b-x-0.5;
+        printf("%0.2f",b);
+    }
+    else {
+        printf("%0.2f",b);
+    }
+
+    return 0;
+    
+}
