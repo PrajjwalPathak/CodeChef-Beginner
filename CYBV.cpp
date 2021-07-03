9d4a2761d605316081b3b7a06995a58cbebf0b99
prajjwalpathak <prajjwalpathak.asm@gmail.com>
2021-07-04 05:20:46 +0530

    Add Programs

---
+++ CYBV.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int t, i, n, k;
+    cin>>t;
+    for(i=0; i<t; i++) {
+        cin>>n>>k;
+        cout<<k/n<<"\n";
+    } 
+    
+    return 0;
+}
